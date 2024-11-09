05d33a9507b506b927921ee2f8820e74415a168b
Julian Maistrovicz <Julianmaistrovicz@gmail.com>
2024-11-09 11:49:18 -0300

    Add files via upload

---
+++ model/MySQLConnector.java
@@ -0,0 +1,27 @@
+
+
+import java.sql.*;
+
+public class MySQLConnector 
+{
+
+    public static Connection conectar() {
+
+        String mysqlHost = "127.0.0.1";
+        String mysqlDb = "db_senac";
+        String mysqlUser = "root";
+        String mysqlPassword = "131313";
+        String mysqlPort = "3306";
+        String mysqlUrl = "jdbc:mysql://" + mysqlHost + ":" + mysqlPort + "/" + 
+        mysqlDb + "?user=" + mysqlUser + "&password=" + mysqlPassword;
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+            conn = DriverManager.getConnection(mysqlUrl); 
+            // System.out.println("Conexão estabelecida!");
+        } catch (Exception e) {
+            System.err.println("Ops! Deu ruim pra cacete ai cumpadi!" +e);
+        }
+        return conn;
+    }
+}
